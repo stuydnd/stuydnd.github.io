7e54c7727b825db8bd76d069f57f79e2a7de917e
boknin <67513101+boknin@users.noreply.github.com>
2020-08-16 16:07:08 -0400

    made width of content change upon menu change

--- planejammer/js/sidebar.js
+++ planejammer/js/sidebar.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
       $('.sidebar').css('width','100px');
       $('.minmaxer').html('+');
       $('.sidebar-contents').css('display','none');
+      $('.content').css('width','80%');
+      $('.content').css('left','10%');
       minimized = true;
     } else {
       $('.sidebar').css('width','390px');
       $('.sidebar-contents').css('display','block');
       $('.minmaxer').html('-');
+      $('.content').css('width','60%');
+      $('.content').css('left','35%');
       minimized = false;
     };
-
   });
 });
